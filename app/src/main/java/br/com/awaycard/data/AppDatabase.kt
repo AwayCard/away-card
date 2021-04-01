52845d68eae3fb1d0fb4d1cdf8db863365eddd96
L. Fernando <faraujo.lfernando@gmail.com>
2021-04-01 10:51:38 -0300

    Intro screen for onboarding app flow (#3)
    feat(onboarding): a structure multi module for app and onboarding

--- app/src/main/java/br/com/awaycard/data/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package br.com.awaycard.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [User::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun userRegisterDAO(): UserRegisterDAO
-}
---
+++ onboarding/src/main/java/br/com/awaycard/onboarding/data/InMemoryUserRegister.kt
@@ -0,0 +1,19 @@
+package br.com.awaycard.onboarding.data
+
+class InMemoryUserRegister(private val appDatabase: AppDatabase) {
+    suspend fun insert(nickname: String) {
+        if (nickname.isNotEmpty()) {
+            return appDatabase.userRegisterDAO().insert(User(nickname = nickname))
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+
+    suspend fun get(): String {
+        return appDatabase.userRegisterDAO().getAll().first().nickname
+    }
+
+    fun onClose() {
+        appDatabase.close()
+    }
+}
