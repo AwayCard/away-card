0e9996b73e8567abdb04c49b981adafe15d9c105
L. Fernando <lfernf@lfernf.local>
2022-04-21 14:31:56 -0300

    Bump 3rd parties and core dependencies

--- onboarding/src/main/java/br/com/awaycard/onboarding/UserRegisterFragment.kt
+++ onboarding/src/main/java/br/com/awaycard/onboarding/UserRegisterFragment.kt
@@ -13,15 +13,17 @@ import br.com.awaycard.onboarding.viewmodel.UserRegisterViewModel
 import br.com.awaycard.platinum.ScreenIndicator
 import br.com.awaycard.platinum.enableFullscreenMode
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class UserRegisterFragment : Fragment() {
     private val viewmodel by viewModels<UserRegisterViewModel>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_user_register, container, false)
     }
 
@@ -31,11 +33,15 @@ class UserRegisterFragment : Fragment() {
         enableFullscreenMode()
 
         requireView().findViewById<ScreenIndicator>(R.id.userRegisterIndicator).setOnClickListener {
-            val input = requireActivity().findViewById<AppCompatEditText>(R.id.userRegisterInput).text.toString()
+            val input =
+                requireActivity().findViewById<AppCompatEditText>(R.id.userRegisterInput).text.toString()
 
             lifecycleScope.launch {
                 viewmodel.execute(input)
-                findNavController().navigate(UserRegisterFragmentDirections.actionUserRegisterBottomSheetToCardRegisterFragment())
+
+                findNavController().navigate(
+                    UserRegisterFragmentDirections.actionUserRegisterBottomSheetToCardRegisterFragment()
+                )
             }
         }
     }
